d799ff1dc39fddb1e61d3d0cfad0c55be85ed61b
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2022-01-05 14:30:53 +0100

    First Commit

---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    // @Value("${app.version}") = will search a propertie called app.version and inject it in the aapVersion attribute.
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping
+    @RequestMapping("/")
+    public Map getStatus() {
+        Map map = new HashMap<String, String>();
+        map.put("app-version", appVersion);
+        return map;
+    }
+}
